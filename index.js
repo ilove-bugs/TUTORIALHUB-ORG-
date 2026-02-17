658f6743a2a3940b06c8388d5de5c570984f84d0
Abhinav khatri <abhinavkhatri25@gmail.com>
2026-02-17 14:03:47 +0545

    fix design, and structures

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// let a= "Find \n the \n best \n tutorials"
+// let l= a.length;
+
+// var i;
+// let delay= ()=>{
+//     return new Promise(resolve=>setInterval(resolve, 900))
+// }
+// let welc= document.querySelector(".welcome")
+// let func=async()=>{
+//     for( i=0;i<=l;i++){
+//       welc.innerHTML+=a[i];
+//  await delay()
+// }
+// }
+// func()
